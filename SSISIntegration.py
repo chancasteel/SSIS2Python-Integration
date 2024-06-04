dcb7f826c4502c5c037afffd3ab92bd911f32d0c
Chandler Casteel <71947560+chancasteel@users.noreply.github.com>
2024-06-04 14:19:41 -0500

    Update SSISIntegration.py

--- SSISIntegration.py
+++ SSISIntegration.py
@@ -1,45 +1,48 @@
 import argparse
+import configparser
+import csv
 import logging
 import os
 import pandas as pd
 from pathlib import Path
 import sqlalchemy
-from sqlalchemy import create_engine, text, URL
 import subprocess
 import shutil
 from zipfile import ZipFile
 import time
 import re
-import csv
 
-###MODIFY VARIABLES BEFORE RUNNING###
-IMPORT_FILES_DIRECTORY = "<import files directory>"
-IMPORT_FILES = ["<file one>", "<file two>"]
-EXPORT_FILE_PATH = "<export file path>"
-EXPORT_FILE_UNC = "<export file network>"
-EXPORT_FILE_DIR = "<export file directory>"
-SQL_QUERY = sqlalchemy.text("""<sql query for export>""")
-###################################
-
-DELIMITER_COMMA = ','
-DELIMITER_PIPE = '|'
-DELIMITER_TAB = '\t'
-HEADER_SHOW = True
-HEADER_HIDE = False
+# Constants to define output requirements
 OUTPUTS_REQUIRED = True
 OUTPUTS_NONE = False
 
-
+# Initial event status and log ID dictionary
 EventStatus = "START"
-EventLogIDs = {}
+
+#Initialize Config File
+config = configparser.ConfigParser()
+config.read('J:\SQL DATA\Scripts\Python\SSIS2Python\config.ini')
+EventLogIDs = dict(config.items('EventLogIds'))
+
+
+# Generate a timestamp string for log file naming
 timestr = time.strftime("%Y%m%d")
 
+# Initialize table and schema names as None
+TABLE_NAME = None
+SCHEMA_NAME = None
+
+# Define the log filename based on the current date
 log_filename = f'Log/SSISIntegration_{timestr}.log'
+
+# Set up the logging configuration
 logging.basicConfig(filename=log_filename, encoding='utf-8', level=logging.DEBUG, format=f'%(asctime)s {EventStatus}: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
+# Log the start of a new event
 logging.info(f"Starting new Event. Event status: {EventStatus}")
 
 def connect_to_database(database_name):
+    """Function to connect to a database using SQLAlchemy"""
     try:
         sql_url = f'mssql+pyodbc://DISD-SQL/{database_name}?trusted_connection=yes&driver=ODBC+Driver+17+for+SQL+Server'
         logging.debug(f'Trying to connect to {database_name} database...')
@@ -49,6 +52,8 @@ def connect_to_database(database_name):
     except OSError as error:
         logging.error(f"ERROR: {error}")
         return None
+
+# Attempt to connect to the "Stage" and "ODS" databases
 try:
     EventStatus = "START"
     connection_stage = connect_to_database("Stage")
@@ -62,15 +67,18 @@ except Exception as error:
     logging.error(f"ERROR at {EventStatus}: {error}")
 
 def importFilesByDirectory(directory : str):
-    """given a directory, search path for files and import them: 
+    """Function to import files from a directory 
     * directory - directory to search"""
     try:
         for filename in os.listdir(directory):
-            importFile(directory, filename)
+            importFile(directory, filename,type='append')
     except OSError as error:
         logging.error(f"Error while listing files in directory {directory}: {error}")
 
-def importFilesByFilePath(filepath, filenames):
+def importFilesByFilePath(filepath, filenames, type=None):
+    """Function to import specific files from a given directory 
+    * filepath - directory to search
+    * filenames - filenames to search for in directory"""
     try:
         for file in filenames:
             importFile(filepath, file)
@@ -78,7 +86,7 @@ def importFilesByFilePath(filepath, filenames):
         logging.exception(f"Error occurred while importing files: {error}")
 
 def importFile(filepath, filename):
-    """given a file path and the filename, check if exists and is compatible to run process: 
+    """Function to import a single file 
     * filepath - full path to file directory
     * filename - name of the specific file"""
     f = os.path.join(filepath, filename)
@@ -92,31 +100,39 @@ def importFile(filepath, filename):
             unzipFiles(file, filename)
 
 def importCSV(filepath, filename):
-    """given a file path and the filename, import the csv file into the connected database with log info and stored procedures: 
+    """Function to import a CSV file into the database 
     * filepath - full path to csv file
     * filename - name of the specific file"""
     logging.debug(f"START {filename}")
     try:
+        if main_args.schema and main_args.table:
+            schema_name = main_args.schema
+            table_name = main_args.table
+        else:
+            db_schema = re.split('[._]', filename)
+            table_name = db_schema[1]
+            schema_name = db_schema[0]
         data_df = pd.read_csv(filepath)
-        db_schema = re.split('[._]', filename)
-        
-        if db_schema[0] == "AD" and (db_schema[1] == "Employee" or db_schema[1] == "Student"):
-            EventLogID = execProcedure(connection_stage.engine, "DECLARE @EventLogID int; EXEC dbo.usp_EventLog_Insert @EventType=?, @EventSubtype=?, @EventStatus=?, @EventLogID = @EventLogID OUTPUT; select @EventLogID as EventLogID;", OUTPUTS_REQUIRED, params=['Import',  filename, EventStatus])
-            EventLogIDs[filename] =  EventLogID     
-            EventStatus = "STAGE"
-            execProcedure(connection_stage.engine, "EXEC dbo.usp_EventLog_Update @EventLogID = ?, @EventStatus = ?;", OUTPUTS_NONE, params=[EventLogIDs[filename], EventStatus])
-            try:
-                table_name = db_schema[1]
-                rowcount = data_df.to_sql(table_name, connection_stage.engine, schema="ad", index=False, if_exists="replace")
-                logging.info(f"{len(data_df)} rows successfully imported to table {db_schema[0]}.{db_schema[1]}")
-            except Exception as error:
-                logging.debug(f"Error while importing data to table ad.{db_schema[1]}: {error}")
-    except:
+
+        EventStatus = "START"
+        EventLogID = execProcedure(connection_stage.engine, "DECLARE @EventLogID int; EXEC dbo.usp_EventLog_Insert @EventType=?, @EventSubtype=?, @EventStatus=?, @EventLogID = @EventLogID OUTPUT; select @EventLogID as EventLogID;", OUTPUTS_REQUIRED, params=['Import',  filename, EventStatus])
+        EventLogIDs[filename] =  EventLogID
+        update_config_file('EventLogIds', EventLogIDs)
+          
+        EventStatus = "STAGE"
+        execProcedure(connection_stage.engine, "EXEC dbo.usp_EventLog_Update @EventLogID = ?, @EventStatus = ?;", OUTPUTS_NONE, params=[EventLogIDs[filename], EventStatus])
+        try:
+            rowcount = data_df.to_sql(table_name, connection_stage.engine, schema=schema_name.lower(), index=False, if_exists=main_args.exists)
+            logging.info(f"{len(data_df)} rows successfully imported to table {schema_name}.{table_name}")
+        except Exception as error:
+            logging.debug(f"Error while importing data to table {schema_name}.{table_name}: {error}")
+    except Exception as error:
         logging.error(f"Error while importing CSV file {filename}: {error}")
     
-    logging.debug(f"OK")
+    logging.debug("OK")
 
 def unzipFiles(filepath, filename):
+    """Function to unzip files and import them"""
     try:
         EventStatus = "TRANSFORM"
         logging.debug(f"{EventStatus} {filename}")
@@ -134,6 +150,7 @@ def unzipFiles(filepath, filename):
         logging.debug(f"{EventStatus}")
 
 def exportFile(filename, csvDelimiter, includeHeader, quoteOption, query, connection):
+    """Function to export a query result to a CSV file"""
     try:
         EventStatus = "START"
         logging.debug(f"{EventStatus}: {filename}")
@@ -142,6 +159,7 @@ def exportFile(filename, csvDelimiter, includeHeader, quoteOption, query, connec
         if base_filename not in EventLogIDs:
             EventLogID = execProcedure(connection_ods.engine, "DECLARE @EventLogID int; EXEC dbo.usp_EventLog_Insert @EventType=?, @EventSubtype=?, @EventStatus=?, @EventLogID = @EventLogID OUTPUT; select @EventLogID as EventLogID;", OUTPUTS_REQUIRED, params=['Export', base_filename, EventStatus])
             EventLogIDs[base_filename] = EventLogID
+            update_config_file('EventLogIDs', EventLogIDs)
         
         EventStatus = "EXPORT"
         logging.debug(f"{EventStatus}: {filename}")
@@ -161,8 +179,8 @@ def exportFile(filename, csvDelimiter, includeHeader, quoteOption, query, connec
     except Exception as error:
         logging.error(f"ERROR at {EventStatus}: {error}")
 
-def execProcedure(engine: sqlalchemy.engine.Engine, procedure: str, outputs: bool, params: list | None = None):
-    """given a stored procedure name and parameters, execute the SQL stored procedure against the connected database with parameters and return the results: 
+def execProcedure(engine: sqlalchemy.engine.Engine, procedure: str, outputs: bool, params: list = None):
+    """Function to execute a stored procedure with optional parameters and output 
     * procedure - the name of the stored procedure to execute
     * outputs - whether the execution procedure returns a value
     * params () - an array of parameters 
@@ -180,6 +198,7 @@ def execProcedure(engine: sqlalchemy.engine.Engine, procedure: str, outputs: boo
     return results[0] if results else None
 
 def copyToUNC(network: os.PathLike, src: os.PathLike, dest: os.PathLike, user: str = None, password: str = None):
+    """Function to copy files to a network location with optional authentication"""
     try:
         if user and password:
             winCMD = f'NET USE {network} /User:{user} {password}'
@@ -192,7 +211,7 @@ def copyToUNC(network: os.PathLike, src: os.PathLike, dest: os.PathLike, user: s
         return None
     
 def archiveFile(file : os.PathLike, archive : os.PathLike):
-    """Send a file to an archive with a date
+    """Function to archive files by appending the current date to the filename
     * file - the file to archive
     * archive - the path to archive directory or folder"""
     try:
@@ -209,47 +228,95 @@ def archiveFile(file : os.PathLike, archive : os.PathLike):
     except Exception as error:
         logging.error(f"Error archiving file: {error}")
         return None
-    
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
 
+def update_config_file(section, config_data):
+    """Updates a specific section of the config.ini file with the provided config_data dictionary.
+
+    Args:
+        section: The name of the section to update in the config file.
+        config_data: A dictionary containing key-value pairs for the specified section.
+    """
+    try:
+        config = configparser.ConfigParser()
+        config.read('J:\SQL DATA\Scripts\Python\SSIS2Python\config.ini')
+
+        for key, value in config_data.items():
+            config.set(section, key, str(value))
+
+        with open('J:\SQL DATA\Scripts\Python\SSIS2Python\config.ini', 'w') as configfile:
+            config.write(configfile)
+    except Exception as error:
+        logging.error(f"Error while updating config file section {section} with {config_data}: {error}")
+    
+
+
+def setup_arg_parser():
+    """Function to set up the argument parser for command-line options"""
+
+    parser = argparse.ArgumentParser(prog='SQL Server SSIS Process')
     subparsers = parser.add_subparsers(dest='type', required=True)
-        
 
-    parser_import = subparsers.add_parser('Import')
-    parser_export = subparsers.add_parser('Export')
-    
-    parser_import.add_argument('-d, --directory', required=True)
-    parser_import.add_argument("-f", "--files", nargs='+')
+    # Subparser for the 'Import' operation
+    parser_import = subparsers.add_parser('Import', help="Import csv file(s) to database")
+    parser_import.add_argument('-d', '--directory', required=True, help="Directory path from which to import file(s)")
+    parser_import.add_argument("-f", "--files", nargs='+', help="Specific filename(s) to import")
+    parser_import.add_argument("--schema", dest='schema', help="Name of schema for import")
+    parser_import.add_argument("--table", dest='table', help="Name of schema for import")
+    parser_import.add_argument("--exists", dest='exists', choices={'replace', 'append'}, default='replace', help="What to do if table exists by default 'replace'")
 
-    parser_export.add_argument("-e, --export", required=True)
-    parser_export.add_argument("-d", "--delimiter", choices=[',','|','\t'], default=',')
-    parser_export.add_argument("-s", '--show', action='store_true')
-    parser_export.add_argument("-q", "--query", required=True)
-    parser_export.add_argument("-n", "--network")
+    # Subparser for the 'Export' operation
+    parser_export = subparsers.add_parser('Export', help="Export query from database to csv")
+    parser_export.add_argument("-e", "--export", required=True, help="Path of file to export")
+    parser_export.add_argument("-del", "--delimiter", choices=[',','|','\t'], default=',', help="Delimiter to use for csv")
+    parser_export.add_argument("-s", '--show', action='store_true', help="Whether to show header")
+    parser_export.add_argument("-q", "--query", required=True, help="SQL Query to export")
 
-    main_args = parser.parse_args()
-    print(main_args)
+    # Subparser for the 'Copy' operation
+    parser_copy = subparsers.add_parser('Copy', help="Send copy of file to another folder/directory")
+    parser_copy.add_argument("-src", "--source", required=True, help="Source file path")
+    parser_copy.add_argument("-dest", "--destination", required=True, help="Destination directory path")
+    parser_copy.add_argument("-n", "--network", help="Path of network/unc")
 
-    try: 
+    return parser.parse_args()
+
+def import_files(directory, files=None):
+    """Function to import files based on directory and specific files if provided"""
+    if files:
+        importFilesByFilePath(directory, files)
+    else:
+        importFilesByDirectory(directory)
+
+def process_import(args):
+    """Function to process the 'Import' command-line option"""
+    IMPORT_FILES_DIRECTORY = args.directory
+    IMPORT_FILES = args.files
+    import_files(IMPORT_FILES_DIRECTORY, IMPORT_FILES)
+
+def process_export(args):
+    """Function to process the 'Export' command-line option"""
+    EXPORT_FILE_PATH = args.export
+    DELIMITER = args.delimiter
+    SHOW_HEADER = args.show
+    SQL_QUERY = args.query
+    exportFile(EXPORT_FILE_PATH, DELIMITER, SHOW_HEADER, csv.QUOTE_ALL, SQL_QUERY, connection_ods)
+
+def process_copy(args):
+    """Function to process the 'Copy' command-line option"""
+    SOURCE_FILE = args.source
+    DEST_DIRECTORY = args.destination
+    FILE_UNC = args.network
+    copyToUNC(FILE_UNC, SOURCE_FILE, DEST_DIRECTORY)
+
+# Main execution entry point
+if __name__ == '__main__':
+    main_args = setup_arg_parser()
+    try:
         if main_args.type.casefold() == 'Import'.casefold():
-            IMPORT_FILES_DIRECTORY = main_args.directory
-            if main_args.files:
-                IMPORT_FILES =  main_args.files
-                importFilesByFilePath(IMPORT_FILES_DIRECTORY, IMPORT_FILES)
-            else:
-                importFilesByDirectory(IMPORT_FILES_DIRECTORY)
+            process_import(main_args)
         elif main_args.type.casefold() == 'Export'.casefold():
-            EXPORT_FILE_PATH =  main_args.export
-            DELIMITER = main_args.delimiter
-            SHOW_HEADER = main_args.show
-            SQL_QUERY = main_args.query
-            if main_args.network:
-                EXPORT_FILE_UNC = main_args.network
-                copyToUNC(EXPORT_FILE_UNC, EXPORT_FILE_PATH, EXPORT_FILE_DIR)
-            else:
-                exportFile(EXPORT_FILE_PATH, DELIMITER, SHOW_HEADER, csv.QUOTE_ALL, SQL_QUERY, connection_ods)        
+            process_export(main_args)
+        elif main_args.type.casefold() == 'Copy'.casefold():
+            process_copy(main_args)
         logging.debug("Process completed successfully.")
-    
-    except:
-        logging.error(f"Error occurred: {error}")
+    except Exception as e:
+        logging.error(f"Error occurred: {e}")
